8693e3efed0d776116576aae09a1390cacad9539
root <andyb@datadevil.co.uk>
2020-06-25 08:38:05 +0100

    Improved comments for eosio.info

--- contracts/eosio.info/include/eosio.info/eosio.info.hpp
+++ contracts/eosio.info/include/eosio.info/eosio.info.hpp
@@ -14,39 +14,71 @@ namespace eosio {
    using std::string;
 
    /**
-    * eosio.info contract defines the structures and actions that allows user KYC metadata to be stored and easily accessed
+    * eosio.info contract defines the structures and actions that allows user KYC metadata to be stored and efficiently accessed
     */
    class [[eosio::contract("eosio.info")]] info : public contract {
       public:
          using contract::contract;
 
          /**
-          * adds a key/verfication definition, called by KYC provider
+          * Adds a key/verfication type definition, called by KYC provider
+          *
+          * @param key - the name of the verification or user info key
+          * @param definition - a text string containing what the key refers to
+          * @param user - whether the key refers to user info (false = verification key)
+          *
+          * @pre key must not already exist in infovertype table
           */
          ACTION addkeytype(const name key, std::string definition, const bool user);
 
          /**
-          * removes a key/verfication definition, called by KYC provider
+          * Removes a key/verfication definition, called by KYC provider
+          *
+          * @param key - the name of the verification or user info key
+          *
+          * @pre key must be present in infovertype table
           */
          //ACTION delkeytype(const name key);
 
          /**
-          * adds a user verfication, called by KYC provider
+          * Adds a user verfication record, called by KYC provider
+          *
+          * @param user - the name of the user account
+          * @param verification_type - the name of the verification key
+          *
+          * @pre verification_type must be a key present in infovertype table
           */
          ACTION adduserver(const name user, const name verification_type);
 
          /**
-          * removes a user verification, called by KYC provider
+          * Removes a user verification, called by KYC provider
+          *
+          * @param user - the name of the user account
+          * @param verification_type - the name of the verification key
+          *
+          * @pre matching record must be present in infouserver table
           */
          // ACTION deluserver(const name user, const name verification_type);
 
          /**
-          * sets a key/verfication offer, called by KYC user
+          * Allows a user to make a key/verfication record available, called by KYC user
+          *
+          * @param user - the name of the user account
+          * @param verification_type - the name of the verification key
+          * @param memo - a text string of up to 1024 characters
+          *
+          * @pre verification_type must be a key present in infovertype table
+          * @pre memo length must not exceed 1024 characters
           */
          ACTION setuserkey(const name user, const name verification_type, const std::string memo);
 
          /**
-          * deletes a key/verfication offer, called by KYC user
+          * Deletes a key/verfication offer, called by KYC user
+          *
+          * @param user - the name of the user account
+          * @param verification_type - the name of the verification key
+          *
+          * @pre record must be present in infouserkey table
           */
          ACTION deluserkey(const name user, const name verification_type);
 
@@ -84,7 +116,7 @@ namespace eosio {
          };
 
          /**
-          * stores the verifications offered by user
+          * stores the keys/verifications provided by user
           */
          struct [[eosio::table]] infouserkey {
             uint64_t      id;
