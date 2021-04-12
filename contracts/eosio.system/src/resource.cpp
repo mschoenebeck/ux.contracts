9093e2189da16e3b46fae33c45e698a532945c89
andybets <andyb@datadevil.co.uk>
2021-04-12 17:09:07 +0100

    Added defensive daily inflation cap

--- contracts/eosio.system/src/resource.cpp
+++ contracts/eosio.system/src/resource.cpp
@@ -235,8 +235,15 @@ namespace eosiosystem {
         bool resource_active = feature_itr == _features.end() ? false : feature_itr->active;
         if(resource_active) {
          {
+            asset inflation = itr_u->bppay_tokens + itr_u->utility_tokens;
+
+            // defensive measure to cap daily inflation at 1.5m UTX
+            asset cap = asset(10000, core_symbol()) * 1500000;
+
+            check(inflation <= cap, "period inflation cap exceeded");
+
             token::issue_action issue_act{token_account, {{get_self(), active_permission}}};
-            issue_act.send(get_self(), itr_u->bppay_tokens + itr_u->utility_tokens, "issue daily inflation");
+            issue_act.send(get_self(), inflation, "issue daily inflation");
          }
          {
             token::transfer_action transfer_act{token_account, {{get_self(), active_permission}}};
